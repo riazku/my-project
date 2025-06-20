77091970d98cf5e1925c1ffbccecab05f31a399a
riaz ahmed <106495345+riazku@users.noreply.github.com>
2025-06-21 02:42:13 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+
 
 export default defineConfig({
     plugins: [
@@ -11,3 +11,5 @@ export default defineConfig({
         tailwindcss(),
     ],
 });
+
+
